f0815907b6f80e395cdc4f59b9b3f186eb4d0606
phmartins89 <ph.martins89@gmail.com>
2020-07-06 00:54:07 -0300

    Tela principal em dev e tela cadastro em html

--- enderecos/views.py
+++ enderecos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+
 
 def index(request):
-    return HttpResponse('<h1>Cadastro</h1>')
+    return render(request,'index.html')
+
+def endereco(request):
+    return render(request,'receita.html')
