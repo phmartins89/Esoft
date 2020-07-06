f3de09c58dc1b07a7327b188f084ba4167309c26
phmartins89 <ph.martins89@gmail.com>
2020-07-06 20:16:00 -0300

    Criação do meu BD_Postgresql

--- sistemaesoft/settings.py
+++ sistemaesoft/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'sistemaesoft.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'meus_enderecos',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost'
     }
 }
 
