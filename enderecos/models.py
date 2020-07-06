5cd170153e0f4bfedd16e08d13b1266fd437af38
phmartins89 <ph.martins89@gmail.com>
2020-07-06 20:52:07 -0300

    Criação das tabelas

--- enderecos/models.py
+++ enderecos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Endereco(models.Model):
+    rua = models.CharField(max_length= 200)
+    bairro = models.CharField(max_length= 200)
+    numero = models.CharField(max_length= 10)
+    complemento = models.CharField(max_length= 200)
+    descricao = models.CharField(max_length= 200)
+    cidade = models.CharField(max_length= 200)
+    uf = models.CharField(max_length= 200)
+    cep = models.CharField(max_length= 200)
+    date_endereco = models.DateTimeField(default = datetime.now, blank= True)
+
+
+
+
+                                       
